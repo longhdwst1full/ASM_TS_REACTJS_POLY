e75e8047d56204589dcfca02c734030a1267f509
dev.fintwin <hoanglonggg356@gmail.com>
2024-04-08 12:33:43 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
-import { useState } from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/User/Home";
 import LayoutUser from "./components/LayoutUser";
-import Login from "./pages/Auth/Login";
 import LayputAdmin from "./pages/Admin/LayputAdmin";
-import ProductsAdmin from "./pages/Admin/Products/ProductsAdmin";
 import ProductAdd from "./pages/Admin/Products/ProductAdd";
-import DetailProduct from "./pages/User/DetailProduct";
-import LayoutCart from "./pages/cart/LayoutCart";
+import ProductsAdmin from "./pages/Admin/Products/ProductsAdmin";
+import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import DetailProduct from "./pages/User/DetailProduct";
+import Home from "./pages/User/Home";
+import LayoutCart from "./pages/cart/LayoutCart";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LayoutUser />}>
-            <Route path="" element={<Home />} />
-            <Route path="product" element={<DetailProduct />} />
-            <Route path=":id/product" element={<DetailProduct />} />
-          </Route>
-          <Route path="/cart" element={<LayoutCart />}>
-            {/* <Route path="" element={<Home />} /> */}
-          </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutUser />}>
+          <Route path="" element={<Home />} />
+          <Route path="product" element={<DetailProduct />} />
+          <Route path=":id/product" element={<DetailProduct />} />
+        </Route>
+        <Route path="/cart" element={<LayoutCart />}>
+          {/* <Route path="" element={<Home />} /> */}
+        </Route>
 
-          <Route path="/admin" element={<LayputAdmin />}>
-            <Route path="" element={<ProductsAdmin />} />
-            <Route path="products" element={<ProductsAdmin />} />
-            <Route path="product-add" element={<ProductAdd />} />
-            <Route path=":id/edit" element={<ProductAdd />} />
-          </Route>
+        <Route path="/admin" element={<LayputAdmin />}>
+          <Route path="" element={<ProductsAdmin />} />
+          <Route path="products" element={<ProductsAdmin />} />
+          <Route path="product-add" element={<ProductAdd />} />
+          <Route path=":id/edit" element={<ProductAdd />} />
+        </Route>
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          {/* <Route path="/product/:id" element={<Home />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/product/:id" element={<Home />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Header() {
+  const [dataUser, setDataUser] = useState();
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setDataUser(JSON.parse(user));
+    }
+  }, []);
+  
   return (
     <header className="bg-red-500">
       <div className="w-[1200px] bg-red-500 mx-auto flex items-center gap-4 justify-center py-2">
         <div className="w-[50px]">
-          <img src="./anhhtus-logo 2.png" alt="" />
+          <Link to={"/"}>
+            <img src="./anhhtus-logo 2.png" alt="" />
+          </Link>
         </div>
         <div className="flex-1">
           <input
@@ -11,6 +24,36 @@ export default function Header() {
             placeholder="Search.."
             type="text"
           />
+        </div>
+        <div className=" flex justify-center gap-2 items-center">
+          <Link to={"/cart"}>
+            <button className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2  ">
+              <img src="../Layer 2.png" alt="" />
+            </button>
+          </Link>
+          {dataUser ? (
+            <>
+              <div className="flex items-center p-2 space-x-4">
+                <img
+                  src="https://source.unsplash.com/100x100/?portrait"
+                  className="w-12 h-12 rounded-full"
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <Link to={"/login"}>
+                <button className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:focus:ring-yellow-900">
+                  Login
+                </button>
+              </Link>
+              <Link to={"/register"}>
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                  Register
+                </button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
